6fe4a086c15e24dedc0314cb6ea7b45e31b0b166
chakawat <59547825+jellypepo02@users.noreply.github.com>
2022-09-12 18:34:02 +0700

    test
    test

--- js/custom.js
+++ js/custom.js
@@ -25,3 +25,24 @@ if (pinnedImageWrappers) {
         });
     });
 }
+
+
+// tab-showcase
+var tabs = document.getElementById('tab-showcase').children;
+var tabcontents = document.getElementById('tab-list-content').children;
+
+var myFunction = function() {
+    var tabchange = this.mynum;
+    for (var int = 0; int < tabcontents.length; int++) {
+        tabcontents[int].className = ' tabcontent';
+        tabs[int].className = ' tab-list';
+    }
+    tabcontents[tabchange].classList.add('tab-active');
+    this.classList.add('current-tab');
+}
+
+
+for (var index = 0; index < tabs.length; index++) {
+    tabs[index].mynum = index;
+    tabs[index].addEventListener('click', myFunction, false);
+}
